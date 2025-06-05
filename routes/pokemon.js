a763b80b52e9d1a6fa89cbe3f09ddac3aad58036
veeforward <160859575+veeforward@users.noreply.github.com>
2025-06-05 15:43:59 +0800

    Fix route and path

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,11 +9,21 @@ const API_URL = process.env.API_URL;
 router.get('/', async (req, res) => {
     try{
         const response = await axios.get(API_URL);
-        res.render("pages/index", { pokemonList: response.data.results });
+        res.render("pages/home", { pokemonList: response.data.results });
     } catch (error) {
         res.status(500).send("Error fetching Pokémon data");
     }
 })
+
+router.get('/pokemon/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        res.render("pages/details", { pokemon: response.data });
+    } catch (error) {
+        res.status(500).send("Error fetching Pokémon data");
+    }
+})   
 
 export default router;
    
